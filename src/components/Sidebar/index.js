2b00745c8fc462d1594cc6a346ce3deaeff748dc
Roman <romandybinetc@gmail.com>
2020-02-07 14:45:09 +0200

    update sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -10,9 +10,25 @@ import Card from '../UI/Card';
 const Sidebar = (props) => {
   return(
     <div className="sidebarConteiner">
-     <Card>
-        Sidebar
+     <Card style={{marginBottom:'20px'}}>
+      <div className="cardHeader">
+        <span>About US</span>
+     </div>
+      <div className>
+        <img src="https://www.google.com/search?q=foldscope&tbm=isch&ved=2ahUKEwjKxqDbt73nAhUHsSoKHX6vCpcQ2-cCegQIABAA&oq=foldscope&gs_l=img.3..0j0i30l9.22228.36530..37228...2.0..0.142.2253.19j5......0....1..gws-wiz-img.....10..35i39j0i24j0i19j0i67j35i362i39.ClpFW8jQ63A&ei=vEo8Xsr4M4fiqgH-3qq4CQ&bih=558&biw=1280#imgrc=uHVPyf84ebdwMM&imgdii=24-dCiow1WK41M"/>
+
+      </div>
      </Card>
+
+     <Card>
+      <div className="cardHeader">
+        <span>Social Network</span>
+     </div>
+     </Card>
+
+
+
+
     </div>
   
    )
--- src/components/UI/Card/index.js
+++ src/components/UI/Card/index.js
@@ -8,7 +8,7 @@ import './style.css';
 
 const Card = (props) => {
   return(
-    <div className="card" style={{width:props.width ? props.width:'100%'}}>
+    <div className="card" style={{width:props.width ? props.width:'100%'}}{...props}>
       {props.children}
     </div>
    )
