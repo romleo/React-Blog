275decd1ee124fc92a49efa9a418917ff6ff0ab7
Roman <romandybinetc@gmail.com>
2020-02-11 14:47:06 +0200

    update3 new section

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import './style.css'; 
+import React, { useState, useEffect } from 'react';
+import './style.css';
 import Card from '../UI/Card';
 import blogPost from '../../data/blog.json';
+
 /**
 * @author
 * @function BlogPost
@@ -9,42 +10,50 @@ import blogPost from '../../data/blog.json';
 
 const BlogPost = (props) => {
 
-  const [post,setPost]=useState({});
+    const [post, setPost] = useState({
+        id: "" ,
+        blogCategory: "" ,
+        blogTitle : "" ,
+        postedOn: "" ,
+        author: "" ,
+        blogImage: "" ,
+        blogText: ""
+    });
+    const [slug, setSlug] = useState('');
+    
+    
+    useEffect(() => {
+        const slug = props.match.params.slug;
+        const post = blogPost.data.find(post => post.slug == slug);
+        setPost(post);
+        setSlug(slug)
+    }, [post, props.match.params.slug]);
 
-    useEffect(()=>{
-     const postId = props.match.params.postId;
-     const post = blogPost.data.find(post=>post.id==postId);
-     setPost(post);
-
-  },post);
+    if(post.blogImage == "") return null;
 
   return(
-    <div className="blogPostConteiner">
-      <Card>
-        <div className="blogHeader">
-          <span className="blogCategory">Featured</span>
-          <h1 className="postTitle">{post.blogTitle}</h1>
-          <span className="postedBy">posted on February 06.2020 by"some author" Blogging Tips</span>
-       </div>
-        <div className="postImageConteiner">
-          <img src={require('../../blogPostImages/foldScopething.jpeg')} alt="Post Image"/>
+        <div className="blogPostContainer">
+            <Card>
+                <div className="blogHeader">
+  <span className="blogCategory">{post.blogCategory}</span>
+                    <h1 className="postTitle">{post.blogTitle}</h1>
+  <span className="postedBy">posted on {post.postedOn} by {post.author}</span>
+                </div>
 
-          <div className="postContent">
-            <h3>Post Title</h3>
-            <p>lorem insum
-              
-            </p>
+                <div className="postImageContainer">
+                    <img src={require('../../blogPostImages/' + post.blogImage)} alt="Post Image" />
+                    
+                </div>
 
-          </div>
-
-
+                <div className="postContent">
+  <h3>{post.blogTitle}</h3>
+  <p>{post.blogText}</p>
+                </div>
+                
+            </Card>
         </div>
-
-
-      </Card>
-    </div>
    )
 
  }
 
-export default BlogPost 
+export default BlogPost
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,34 +1,36 @@
-import React ,{useState,useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Card from '../UI/Card';
-import blogPost from '../../data/blog.json'
+
+import blogPost from '../../data/blog.json';
+import { NavLink } from 'react-router-dom';
 
 /**
 * @author
-* @function Sidebare
+* @function Sidebar
 **/
 
 const Sidebar = (props) => {
 
-  const [posts,setPost]=useState([]);
-
-  useEffect(()=>{
-   
-   const posts = blogPost.data ;
-   setPost(posts);
-
-},posts);
- 
+    const [posts, setPosts] = useState([]);
+    
+    
+    useEffect(() => {
+        const posts = blogPost.data;
+        setPosts(posts);
+    }, [posts]);
 
 
 
   return(
-    <div className="sidebarConteiner">
-     <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
-      <div className="cardHeader">
-        <span>About US</span>
-      </div>
-      <div className="profileImageConteiner">
+    <div className="sidebarContainer" style={{
+      width: props.width
+  }}>
+        <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+            <div className="cardHeader">
+                <span>About Us</span>
+            </div>
+            <div className="profileImageContainer">
         <img src="https://3krwor3xbp5c45ck4n3d5u2x-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/flodscoprimg-3.jpg"alt=""/>
       </div>
       <div className="cardBody">
@@ -50,23 +52,22 @@ const Sidebar = (props) => {
       <div className="recentPost">
 
         {
-          posts.map(post =>{
-            return(
-             <div className="recentPost">
-               <h3>{post.blogTitle}</h3>
-               <span>{post.postedOn}</span>
-            </div>
+           posts.map(post => {
+            return (
+                <NavLink key={post.id} to={`/post/${post.slug}`}>
+                    <div className="recentPost">
+                        <h3>{post.blogTitle}</h3>
+                        <span>{post.postedOn}</span>
+                    </div>
+                </NavLink>
+              
 
+              
 
             );
           })
         }
-        
-
-        
-       
-
-      </div>
+       </div>
 
      </Card>
    </div>
