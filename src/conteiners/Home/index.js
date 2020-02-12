85ddcf449fe2a6160bbaf3078e38ea94685e7d8b
Roman <romandybinetc@gmail.com>
2020-02-12 12:10:17 +0200

    create section Home/Posts

--- src/conteiners/Home/index.js
+++ src/conteiners/Home/index.js
@@ -19,8 +19,10 @@ const Home = props =>{
             <img scr={"https://images.squarespace-cdn.com/content/v1/57bccd1f3e00bea28d5766aa/1508967280636-MG4U0E41Q77RHSCB74NJ/ke17ZwdGBToddI8pDm48kO7Rg1Gpu728H4UqxUIfecJZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PICj5SdZHY9KUswDgTH3eH8sP5PrkY15Dr7CE2CPSRiTEKMshLAGzx4R3EDFOm1kBS/IMG_0556+2.jpg"}alt=""/>
           </div>
         </section>
-        <section style={{width:'70%'}}>
-          less popupar Post
+        <section style={{width:'30%'}}>
+        <div>
+            <img scr={"https://images.squarespace-cdn.com/content/v1/57bccd1f3e00bea28d5766aa/1508967280636-MG4U0E41Q77RHSCB74NJ/ke17ZwdGBToddI8pDm48kO7Rg1Gpu728H4UqxUIfecJZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PICj5SdZHY9KUswDgTH3eH8sP5PrkY15Dr7CE2CPSRiTEKMshLAGzx4R3EDFOm1kBS/IMG_0556+2.jpg"}alt=""/>
+          </div>
         </section>
         </div>
 
