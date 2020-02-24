bbfc29dbe610978169deb7ca005e5ccd9f39b82a
Roman <romandybinetc@gmail.com>
2020-02-24 11:44:14 +0200

    fixed bags

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -9,6 +9,7 @@ import './style.css';
 const Logo = (props) => {
   return(
     <div className="logo">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNJffP6iMZQ4KRd2rCOTChSiOHLobtjzKxGNGqzex-c6uekUdA" className="App-logo" alt="logo" />
      <a href="#">Fold Scope</a>
     </div>
    )
